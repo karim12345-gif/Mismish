b9c2535de60d3f1ff1c7a4df001992204298e089
karim tariq salim <62877005+karim12345-gif@users.noreply.github.com>
2026-02-01 21:33:59 +0300

    fix the safe area provider issue

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { AuthProvider } from "./src/context/AuthContext";
